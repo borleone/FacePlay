0c7abdf3d9a424e7081608153cc148cc8141048d
borleone <pushkar.borle@gmail.com>
2015-11-15 08:49:21 -0500

    Music player

--- app/src/main/java/com/borleone/faceplay/MusicList.java
+++ app/src/main/java/com/borleone/faceplay/MusicList.java
@@ -1,11 +1,17 @@
 package com.borleone.faceplay;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -22,18 +28,21 @@ import java.util.TimeZone;
  * Created by Pushkar Borle on 11/14/2015.
  */
 public class MusicList extends Activity {
-    // All static variables
-    static final String URL = "http://api.androidhive.info/music/music.xml";
-    // XML node keys
-    static final String KEY_SONG = "song"; // parent node
+
     static final String KEY_ID = "id";
     static final String KEY_TITLE = "title";
     static final String KEY_ARTIST = "artist";
     static final String KEY_DURATION = "duration";
     static final String KEY_THUMB_URL = "thumb_url";
 
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound = false;
+
     ListView list;
     LazyAdapter adapter;
+    private ArrayList<Song> songList = new ArrayList<Song>();
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,10 +84,20 @@ public class MusicList extends Activity {
 
                 // adding HashList to ArrayList
                 songsList.add(map);
-
+                songList.add(new Song(thisId, thisTitle, thisArtist));
             }
             while (musicCursor.moveToNext());
         }
+        Log.e("songList", String.valueOf(songList.size()));
+
+
+/*
+        Collections.sort(songsList, new Comparator<String>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+*/
 
         list = (ListView) findViewById(R.id.list);
 
@@ -91,8 +110,48 @@ public class MusicList extends Activity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                System.out.println(id + " " + position + " " + view.toString());
+                musicSrv.setSong(position);
+                musicSrv.playSong();
             }
         });
+    }
+
+    //connect to the service
+    private ServiceConnection musicConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder) service;
+            //get service
+            musicSrv = binder.getService();
+            //pass list
+            Log.e("Service songList", String.valueOf(songList.size()));
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
+    //start and bind the service when the activity starts
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (playIntent == null) {
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(playIntent);
+        musicSrv = null;
+        super.onDestroy();
     }
 }
