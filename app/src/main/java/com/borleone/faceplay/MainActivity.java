f722900236055067dcc07e2535aaacbbec183284
borleone <pushkar.borle@gmail.com>
2015-11-14 20:48:37 -0500

    Image capture and save locally

--- app/src/main/java/com/borleone/faceplay/MainActivity.java
+++ app/src/main/java/com/borleone/faceplay/MainActivity.java
@@ -1,31 +1,32 @@
 package com.borleone.faceplay;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        Button Button = (Button) this.findViewById(R.id.button);
+        Button.setOnClickListener(new View.OnClickListener() {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent k = new Intent(MainActivity.this, MoodDetect.class);
+                startActivity(k);
             }
         });
+
     }
 
     @Override
