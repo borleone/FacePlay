77c173d84b3e01a3bfb5e1f4831c63ea4c907485
Ajay <ajay191191@gmail.com>
2015-11-15 02:03:40 -0500

    Merging.

--- app/src/main/java/com/borleone/faceplay/MoodDetect.java
+++ app/src/main/java/com/borleone/faceplay/MoodDetect.java
@@ -1,7 +1,9 @@
 package com.borleone.faceplay;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -10,13 +12,21 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.faceplay.emotion.ProjectOxfordEmotionRecognizer;
+
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -30,9 +40,18 @@ public class MoodDetect extends Activity {
     private static final int CAMERA_REQUEST = 1;
     Uri fileUri = null;
     private ImageView iv = null;
+    private static final int REQUEST_WRITE_STORAGE = 112;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        boolean hasPermission = (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (hasPermission)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_STORAGE);
+        }
         setContentView(R.layout.mood_detect);
         iv = (ImageView) findViewById(R.id.imageView1);
         Button camButton = (Button) this.findViewById(R.id.button);
@@ -85,6 +104,13 @@ public class MoodDetect extends Activity {
                             Toast.LENGTH_LONG).show();
                 }
                 showPhoto(photoUri);
+                ProjectOxfordEmotionRecognizer recog = new ProjectOxfordEmotionRecognizer();
+                try {
+                    recog.submitImage(new URL(photoUri.getEncodedPath()));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(this.getParent(), recog.getEmotion().getEmotion(), Toast.LENGTH_LONG).show();
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
             } else {
@@ -108,5 +134,23 @@ public class MoodDetect extends Activity {
             iv.setImageDrawable(drawable);  //setImageBitmap(Bitmap)
             iv.setRotation(-90);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case REQUEST_WRITE_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    //reload my activity with permission granted
+                } else
+                {
+                    Toast.makeText(this.getParent(), "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
     }
 }
